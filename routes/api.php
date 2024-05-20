fbafea5bad803448f8d32bea41b04765d2788b5c
rakib <phi.rakib@gmail.com>
2024-05-20 21:39:35 +0600

    installed laravel sanctum

---
+++ routes/api.php
@@ -0,0 +1,9 @@
+<?php
+
+use App\Http\Controllers\AccountController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
