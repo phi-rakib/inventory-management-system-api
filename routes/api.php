e8f52fc34a54ac7c10b713c63806ea9eb6002774
rakib <phi.rakib@gmail.com>
2024-05-21 13:18:21 +0600

    added login route & sanctum auth middleware

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,13 @@
 <?php
 
 use App\Http\Controllers\AccountController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('login', [AuthController::class, 'login']);
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::resource('accounts', AccountController::class);    
+});
+
