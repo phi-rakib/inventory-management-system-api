7dab7c754b9fa23753b503fbd12bfe4c821fa23e
rakib <phi.rakib@gmail.com>
2024-06-01 19:57:00 +0600

    removed slug, status, created_at, updated_at, deleted_by from fillable property

--- app/Models/PaymentMethod.php
+++ app/Models/PaymentMethod.php
@@ -4,18 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class PaymentMethod extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'name',
-        'slug',
-        'status',
-        'created_by',
-        'updated_by',
-        'deleted_by',
     ];
 
     public function deposits()
