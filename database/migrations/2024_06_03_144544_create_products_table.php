09618580b118f12bcf6634e7b2d8f3b84e6cf8c6
rakib <phi.rakib@gmail.com>
2024-06-04 16:10:41 +0600

    added column slug in products table

--- database/migrations/2024_06_03_144544_create_products_table.php
+++ database/migrations/2024_06_03_144544_create_products_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('slug')->unique();
             $table->text('description')->nullable();
             $table->enum('status', ['active', 'inactive'])->default('active');
             $table->string('image')->nullable();
