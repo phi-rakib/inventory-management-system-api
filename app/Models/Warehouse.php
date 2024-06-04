f4b936d4d5219ea49845128e2fbc5750e06dfe5d
rakib <phi.rakib@gmail.com>
2024-06-04 16:11:53 +0600

    added products relationship in warehouse model

--- app/Models/Warehouse.php
+++ app/Models/Warehouse.php
@@ -34,6 +34,6 @@ class Warehouse extends Model
 
     public function products()
     {
-        return $this->belongsToMany(Product::class);
+        return $this->belongsToMany(Product::class)->withPivot(['quantity']);
     }
 }
