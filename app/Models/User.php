5945dca9903f3644f986b0043e149f8bde06ddd0
rakib <phi.rakib@gmail.com>
2024-06-06 12:16:03 +0600

    added supplier relationship in user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,4 +46,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function supplier()
+    {
+        return $this->hasOne(Supplier::class);
+    }
 }
