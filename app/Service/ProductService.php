cb74bd54c15d4eccf5ccbf68b7794d1f3a767096
rakib <phi.rakib@gmail.com>
2024-06-08 00:08:44 +0600

    code refractor: moved business logic product controller to service

---
+++ app/Service/ProductService.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Service;
+
+use App\Models\Price;
+use App\Models\Product;
+use Illuminate\Support\Facades\DB;
+
+class ProductService
+{
+    public function store($data)
+    {
+        DB::transaction(function () use ($data) {
+            $product = Product::create($data);
+
+            $product->prices()->create(['price' => $data['price']]);
+
+            $product->attributes()->attach($data['attributes']);
+        });
+    }
+
+    public function update($data, $product)
+    {
+        DB::transaction(function () use ($data, $product) {
+            $product->update($data);
+
+            $product->attributes()->sync($data['attributes']);
+
+            $latestPrice = $product->latestPrice;
+
+            if ($latestPrice == null || $latestPrice->price != $data['price']) {
+                Price::create([
+                    'product_id' => $product->id,
+                    'price' => $data['price'],
+                ]);
+            }
+        });
+    }
+
+    public function destroy($product)
+    {
+        DB::transaction(function () use ($product) {
+            $product->deleted_by = auth()->id();
+            $product->save();
+
+            $product->delete();
+        });
+    }
+}
