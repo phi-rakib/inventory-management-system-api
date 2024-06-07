9aa75c25248ce0bf75960f5f6b56e3d858fca007
rakib <phi.rakib@gmail.com>
2024-06-08 00:20:35 +0600

    used sync instead of attach when creating product attributes

--- app/Service/ProductService.php
+++ app/Service/ProductService.php
@@ -7,35 +7,38 @@ use Illuminate\Support\Facades\DB;
 
 class ProductService
 {
-    public function store($data)
+    public function store(array $productData)
     {
-        DB::transaction(function () use ($data) {
-            $product = Product::create($data);
+        DB::transaction(function () use ($productData) {
+            $product = Product::create($productData);
 
-            $product->prices()->create(['price' => $data['price']]);
+            $product->prices()->create([
+                'price' => $productData['price'],
+            ]);
 
-            $product->attributes()->attach($data['attributes']);
+            $product->attributes()->sync($productData['attributes']);
         });
     }
 
-    public function update($data, $product)
+    public function update(array $productData, Product $product)
     {
-        DB::transaction(function () use ($data, $product) {
-            $product->update($data);
+        DB::transaction(function () use ($productData, $product) {
 
-            $product->attributes()->sync($data['attributes']);
+            $product->update($productData);
+
+            $product->attributes()->sync($productData['attributes']);
 
             $latestPrice = $product->latestPrice;
 
-            if ($latestPrice == null || $latestPrice->price != $data['price']) {
+            if ($latestPrice === null || $latestPrice->price != $productData['price']) {
                 $product->prices()->create([
-                    'price' => $data['price'],
+                    'price' => $productData['price'],
                 ]);
             }
         });
     }
 
-    public function destroy($product)
+    public function destroy(Product $product)
     {
         DB::transaction(function () use ($product) {
             $product->deleted_by = auth()->id();
