2677788f6b2c420ed1f03a0b39ed08ba8ed0a18f
rakib <phi.rakib@gmail.com>
2024-06-12 01:04:23 +0600

    used fill method in AdjustmentObserver

--- app/Observers/AdjustmentObserver.php
+++ app/Observers/AdjustmentObserver.php
@@ -8,11 +8,11 @@ class AdjustmentObserver
 {
     public function creating(Adjustment $adjustment): void
     {
-        $adjustment->created_by = (int) auth()->user()->id;
+        $adjustment->fill(['created_by' => auth()->id()]);
     }
 
     public function updating(Adjustment $adjustment): void
     {
-        $adjustment->updated_by = (int) auth()->user()->id;
+        $adjustment->fill(['updated_by' => auth()->id()]);
     }
 }
