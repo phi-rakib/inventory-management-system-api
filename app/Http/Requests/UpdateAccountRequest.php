9e25510c27384d09eed6c4c74f82230e496c9daa
rakib <phi.rakib@gmail.com>
2024-06-16 10:22:08 +0600

    added updateAccountRequest

---
+++ app/Http/Requests/UpdateAccountRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Gate;
+
+class UpdateAccountRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return Gate::allows('update', $this->account);
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|max:255',
+            'description' => 'nullable',
+            'status' => 'required|in:active,inactive',
+        ];
+    }
+}
