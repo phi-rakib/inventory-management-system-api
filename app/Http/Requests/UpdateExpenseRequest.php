02d581f7808cb7b8f0723e553f927a7be17b8290
rakib <phi.rakib@gmail.com>
2024-06-16 20:38:55 +0600

    added updateExpenseRequest & moved Expense store & update method's authorization logic to request class

--- app/Http/Requests/UpdateExpenseRequest.php
+++ app/Http/Requests/UpdateExpenseRequest.php
@@ -2,18 +2,17 @@
 
 namespace App\Http\Requests;
 
-use App\Models\Expense;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Support\Facades\Gate;
 
-class StoreExpenseRequest extends FormRequest
+class UpdateExpenseRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
      */
     public function authorize(): bool
     {
-        return Gate::allows('create', Expense::class);
+        return Gate::allows('update', $this->expense);
     }
 
     /**
