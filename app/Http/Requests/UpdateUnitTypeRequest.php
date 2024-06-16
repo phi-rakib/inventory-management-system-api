03d10ceadc4f8de7d4fdffaef70be71884ec6b04
rakib <phi.rakib@gmail.com>
2024-06-16 21:10:36 +0600

    added updateUnitTypeRequest & moved UnitType store & update method's authorization logic to request class

--- app/Http/Requests/UpdateUnitTypeRequest.php
+++ app/Http/Requests/UpdateUnitTypeRequest.php
@@ -2,18 +2,17 @@
 
 namespace App\Http\Requests;
 
-use App\Models\UnitType;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Support\Facades\Gate;
 
-class StoreUnitTypeRequest extends FormRequest
+class UpdateUnitTypeRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
      */
     public function authorize(): bool
     {
-        return Gate::allows('create', UnitType::class);
+        return Gate::allows('update', $this->unitType);
     }
 
     /**
