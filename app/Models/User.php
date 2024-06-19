85c071728d4a4052c13d1ace6bf854241990b1b8
rakib <phi.rakib@gmail.com>
2024-06-19 23:02:57 +0600

    added array type

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,7 +22,7 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
-    protected $fillable = [
+    protected array $fillable = [
         'name',
         'email',
         'password',
@@ -33,10 +33,20 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
-    protected $hidden = [
+    protected array $hidden = [
         'password',
         'remember_token',
     ];
+
+    public function supplier(): HasOneThrough
+    {
+        return $this->hasOneThrough(Supplier::class, Account::class);
+    }
+
+    public function account(): HasOne
+    {
+        return $this->hasOne(Account::class);
+    }
 
     /**
      * Get the attributes that should be cast.
@@ -49,15 +59,5 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
-    }
-
-    public function supplier(): HasOneThrough
-    {
-        return $this->hasOneThrough(Supplier::class, Account::class);
-    }
-
-    public function account(): HasOne
-    {
-        return $this->hasOne(Account::class);
     }
 }
